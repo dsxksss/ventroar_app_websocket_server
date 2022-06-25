ac3170298f4e8d8231346222a759512c617df77e
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-06-25 23:00:19 +0800

    add:实现基本websocket功能

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () =>
-  console.log(`Server running at  http://127.0.0.1:${port}`)
-);
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
+
+io.on("connection", socket => {
+  socket.on("chat message", msg => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
+});
